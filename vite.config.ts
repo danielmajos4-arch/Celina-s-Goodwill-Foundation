a67604b4801f5d21ff935fda4cc30a825d8123ab
danielmajos4-arch <danielmajos4@gmail.com>
2025-09-19 08:40:12 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: "client", // tells Vite that index.html is inside client/
+  root: "client",
   build: {
-    outDir: "../dist", // puts the build at project root /dist
+    outDir: "../dist",
     emptyOutDir: true,
   },
   plugins: [react()],
